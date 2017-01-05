3e2c0ff49426277e0fd06599acd8fb908e776093
martin_v <martin.van-elslande@epitech.eu>
2017-01-05 15:35:51 +0100

    rc

--- src/raycast.c
+++ src/raycast.c
@@ -5,7 +5,7 @@
 ** Login   <martin.van-elslande@epitech.eu>
 ** 
 ** Started on  Thu Dec 15 16:19:17 2016 Martin Van Elslande
-** Last update Thu Jan  5 15:30:54 2017 Martin Van Elslande
+** Last update Thu Jan  5 15:34:43 2017 Martin Van Elslande
 */
 
 #include	"wolf3d.h"
@@ -20,13 +20,13 @@ float		raycast(sfVector2f pos, float direction, int **map,
   moves.x = pos.x;
   moves.y = pos.y;
   while ((int)(moves.x) < mapSize.x && (int)(moves.y) < mapSize.y &&
-	 moves.x >= 0.0 && moves.y >= 0.0)
+	 moves.x >= 0.0f && moves.y >= 0.0f)
     {
-      moves = move_forward(moves, direction, 0.01f);
-      distance += 0.01f;
-      if ((int)(moves.x + 0.01f) < mapSize.x &&
-	  (int)(moves.y + 0.01f) < mapSize.y &&
-	  map[(int)(moves.y + 0.01f)][(int)(moves.x + 0.01f)] == 1)
+      moves = move_forward(moves, direction, 0.001f);
+      distance += 0.001f;
+      if ((int)(moves.x + 0.001f) < mapSize.x &&
+	  (int)(moves.y + 0.001f) < mapSize.y &&
+	  map[(int)(moves.y + 0.001f)][(int)(moves.x + 0.001f)] == 1)
 	return (distance);
     }
   return (distance);
