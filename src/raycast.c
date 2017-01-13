53010cf5b447b54c728faead03c0dfcb4440fb6a
martin_v <martin.van-elslande@epitech.eu>
2017-01-13 16:13:38 +0100

    inchallah

--- src/raycast.c
+++ src/raycast.c
@@ -5,7 +5,7 @@
 ** Login   <martin.van-elslande@epitech.eu>
 ** 
 ** Started on  Thu Dec 15 16:19:17 2016 Martin Van Elslande
-** Last update Fri Jan 13 16:05:04 2017 Martin Van Elslande
+** Last update Fri Jan 13 16:12:47 2017 Martin Van Elslande
 */
 
 #include	"wolf3d.h"
@@ -16,14 +16,12 @@ float		raycast(sfVector2f pos, float direction, int **map,
   sfVector2f	moves;
   float		distance;
 
+  (void)mapSize;
   distance = 0.0f;
   moves.x = pos.x;
   moves.y = pos.y;
-  while (map[(int)(moves.y + 0.0001f)][(int)(moves.x + 0.0001f)] == 0)
-    {
-      moves = move_forward(moves, direction, 0.0001f);
-      distance += 0.0001f;
-    }
+  while (map[(int)moves.y][(int)moves.x] == 0)
+    moves = move_forward(moves, direction, 0.001f);
   distance = sqrt(pow(fabs(moves.x - pos.x), 2) + pow(fabs(moves.y - pos.y), 2));
   return (distance);
 }
